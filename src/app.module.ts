ca46c174b4e48b7b5ddc96f8ae78561da524514c
Eddleyva84 <eduardo.leyva@socialreality.com>
2024-07-03 17:48:24 -0700

    pokedex

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,9 @@ import { ConfigModule } from '@nestjs/config';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGODB, {}),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName:'pokemonsdb'
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
--- src/main.ts
+++ src/main.ts
@@ -17,5 +17,6 @@ async function bootstrap() {
     }),
   );
   await app.listen(process.env.PORT);
+  console.log(`App running in the port ${process.env.PORT}`);
 }
 bootstrap();
